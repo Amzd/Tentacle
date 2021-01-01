7b65a653af790e5651f7e91a266c93c48b0458f7
Amzd <info@casperzandbergen.nl>
2021-01-01 13:55:46 +0100

    Update Color.swift

--- Sources/Tentacle/Color.swift
+++ Sources/Tentacle/Color.swift
@@ -11,22 +11,24 @@ import Foundation
 #if os(iOS) || os(tvOS)
     import UIKit
     public typealias Color = UIColor
-#else
+#elseif os(macOS)
     import Cocoa
     public typealias Color = NSColor
 #endif
 
-extension Color {
-    internal convenience init(hex: String) {
-        precondition(hex.count == 6)
+#if os(iOS) || os(tvOS) || os(macOS)
+    extension Color {
+        internal convenience init(hex: String) {
+            precondition(hex.count == 6)
 
-        let scanner = Scanner(string: hex)
-        var rgb: UInt32 = 0
-        scanner.scanHexInt32(&rgb)
+            let scanner = Scanner(string: hex)
+            var rgb: UInt32 = 0
+            scanner.scanHexInt32(&rgb)
 
-        let r = CGFloat((rgb & 0xff0000) >> 16) / 255.0
-        let g = CGFloat((rgb & 0x00ff00) >>  8) / 255.0
-        let b = CGFloat((rgb & 0x0000ff) >>  0) / 255.0
-        self.init(red: r, green: g, blue: b, alpha: 1)
+            let r = CGFloat((rgb & 0xff0000) >> 16) / 255.0
+            let g = CGFloat((rgb & 0x00ff00) >>  8) / 255.0
+            let b = CGFloat((rgb & 0x0000ff) >>  0) / 255.0
+            self.init(red: r, green: g, blue: b, alpha: 1)
+        }
     }
-}
+#endif
